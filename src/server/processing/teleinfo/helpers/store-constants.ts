2471e22616ea67232ed2430ba47fec75fcff7624
djey47 <djey47@github.com>
2023-02-10 23:59:33 +0100

    feature(processing): create data store keys for indexes/costs/supply over a month

--- src/server/processing/teleinfo/helpers/store-constants.ts
+++ src/server/processing/teleinfo/helpers/store-constants.ts
@@ -1,11 +1,32 @@
 /* Data store keys or key prefixes */
 
+/** Timestamps for the moment when data has been first received */
 export const FIRST_DATA_TS_IS_KEY = 'FIRST_DATA_TIMESTAMP';
 
+/** Indexes when the data has been first received */
 export const INITIAL_INDEXES_IS_KEY = 'INITIAL_INDEXES';
+
+/** Total costs from the first data received until present */
 export const TOTAL_COSTS_IS_KEY = 'TOTAL_COSTS';
+
+/** Total supplied energy from the first data received until present */
 export const TOTAL_SUPPLIED_IS_KEY = 'TOTAL_SUPPLIED';
 
+/** Indexes at the beginning of the day */
 export const PER_DAY_INDEXES_IS_KEY_PREFIX = 'DAY_INDEXES_';
+
+/** Total costs for the day */
 export const PER_DAY_COSTS_IS_KEY_PREFIX = 'DAY_COSTS_';
+
+/** Supplied energy for the day */
 export const PER_DAY_SUPPLIED_IS_KEY_PREFIX = 'DAY_SUPPLIED_';
+
+/** Indexes at the beginning of the month */
+export const PER_MONTH_INDEXES_IS_KEY_PREFIX = 'MONTH_INDEXES_';
+
+/** Total costs for the month */
+export const PER_MONTH_COSTS_IS_KEY_PREFIX = 'MONTH_COSTS_';
+
+/** Supplied energy for the month */
+export const PER_MONTH_SUPPLIED_IS_KEY_PREFIX = 'MONTH_SUPPLIED_';
+
