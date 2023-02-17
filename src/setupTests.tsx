0957a3dac428865856551a1fa96daf72abcc5dd1
djey47 <djey47@github.com>
2023-02-17 17:13:41 +0100

    test(module): add setupTests script

---
+++ src/setupTests.tsx
@@ -0,0 +1,17 @@
+import { FontAwesomeIconProps } from "@fortawesome/react-fontawesome";
+
+// global mocks
+
+// font-awesome
+jest.mock('@fortawesome/react-fontawesome', () => ({
+  FontAwesomeIcon: (props: FontAwesomeIconProps) => <div>FontAwesomeIcon component {JSON.stringify(props)}</div>,
+}));
+jest.mock('@fortawesome/free-regular-svg-icons', () => ({
+  faCircleQuestion: 'faCircleQuestion',
+}));
+jest.mock('@fortawesome/free-solid-svg-icons', () => ({
+  faBoltLightning: 'faBoltLightning',
+  faLeaf: 'faLeaf',
+  faPlugCircleBolt: 'faPlugCircleBolt',
+  faPlugCircleExclamation: 'faPlugCircleExclamation',
+}));
