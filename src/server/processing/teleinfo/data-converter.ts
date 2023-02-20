b7d56358263a576c7176f4c482d209b1e32c9c25
djey47 <djey47@github.com>
2023-02-20 22:58:01 +0100

    feature(processing): remove dots from chosen fare option value

--- src/server/processing/teleinfo/data-converter.ts
+++ src/server/processing/teleinfo/data-converter.ts
@@ -23,12 +23,12 @@ export function convertTeleinfoRawData(
     case TeleInfoHistoricalOutputKeys.maxCalledIntensity.toString():
     case TeleInfoHistoricalOutputKeys.subscribedIntensity.toString():
     case TeleInfoHistoricalOutputKeys.subscribedPowerOverflowWarning.toString():
-        return Number(rawValue);
+      return Number(rawValue);
     case TeleInfoHistoricalOutputKeys.chosenFareOption.toString():
+    case TeleInfoHistoricalOutputKeys.currentFarePeriod.toString():
       return rawValue.replace(/\./g, '');
     case TeleInfoHistoricalOutputKeys.counterAddress.toString():
     case TeleInfoHistoricalOutputKeys.counterStateWord.toString():
-    case TeleInfoHistoricalOutputKeys.currentFarePeriod.toString():
     case TeleInfoHistoricalOutputKeys.ejpNotice.toString():
     case TeleInfoHistoricalOutputKeys.lowHighHoursSchedule.toString():
       return rawValue;
