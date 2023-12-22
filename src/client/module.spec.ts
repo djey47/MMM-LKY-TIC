50ca16b77adac01d8daaa6dd2b6cc8f13adaf1ce
djey47 <djey47@github.com>
2023-12-22 11:56:51 +0100

    test(module): update name

--- src/client/module.spec.ts
+++ src/client/module.spec.ts
@@ -44,7 +44,7 @@ describe('MM2 Module client', () => {
     const { name, implementation } = checkAndExtractRegistration(
       mockModuleRegister.mock.lastCall
     );
-    expect(name).toBe('mmm-LKY-TIC');
+    expect(name).toBe('mmm-lky-tic');
     expect(implementation.defaults).toEqual({
       currencySymbol: '€',
       debug: false,
