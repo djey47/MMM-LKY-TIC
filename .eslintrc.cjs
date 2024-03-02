5dd4e89a31af9bd6417cb08e022b1f5fc2c473a2
djey47 <djey47@github.com>
2024-03-02 15:34:12 +0100

    chore(eslint): enable singlequotes rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,4 +3,7 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint', 'react', 'react-hooks'],
   root: true,
+  rules: {
+    quotes: ['warn', 'single'],
+  }
 };
