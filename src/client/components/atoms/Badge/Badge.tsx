aea9011b28ea0c82eb50914c2b4e7e76e3884f77
djey47 <djey47@github.com>
2025-05-01 21:52:15 +0200

    feature(client): make badge clickable with active flag

--- src/client/components/atoms/Badge/Badge.tsx
+++ src/client/components/atoms/Badge/Badge.tsx
@@ -1,13 +1,30 @@
+import clsx from 'clsx';
+import { useCallback } from 'react';
 import './Badge.scss';
 
 export interface BadgeProps {
+  active?: boolean;
+  onClick?: () => void;
   text: string;
 };
 
-const Badge: React.FC<BadgeProps> = (props: BadgeProps) => {
+const Badge = ({ active = false, onClick, text }: BadgeProps) => {
+  const handleKeyDown = useCallback((e: React.KeyboardEvent) => {
+    if (e.key === 'Enter' && onClick) {
+      onClick();
+    }
+  }, [onClick]);
+  
+  const className = clsx('badge', { 'badge--active': active, 'badge--clickable': !!onClick });
   return (
-    <span className="badge">
-      {props.text}
+    <span
+      role="button"
+      tabIndex={0}
+      onKeyDown={handleKeyDown}
+      aria-hidden="true"
+      className={className} 
+      onClick={onClick}>
+      {text}
     </span>
   );
 };
